c39779af02c092bdd895a1fc9b33735b80d574e6
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-15 11:59:11 +0100

    Setup scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Header from "./components/Header";
-import Students from "./pages/Students";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import C404 from "./pages/404";
-import ViewStudent from "./pages/ViewStudent";
+import Header from './components/Header';
+import Students from './pages/Students';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import C404 from './pages/404';
+import ViewStudent from './pages/ViewStudent';
 
-function App(props) {
-  return (
-    <BrowserRouter>
-      <div className="urbanist app">
-        <Header />
-        <div className="routes">
-          <Routes>
-            <Route path="/" element={<Students />} />
-            <Route path="/students/:id/*" element={<ViewStudent />} />
-            <Route path="*" element={<C404 />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+function App() {
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<Header />
+				<div className="routes">
+					<Routes>
+						<Route path="/" element={<Students />} />
+						<Route path="/students/:id/*" element={<ViewStudent />} />
+						<Route path="*" element={<C404 />} />
+					</Routes>
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
