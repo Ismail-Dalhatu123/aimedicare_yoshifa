b4331689195184a2f48c2cf380a6897209258fa4
Ahmad Habib <ahmadalkali69@gmail.com>
2022-08-17 16:36:07 +0100

    Refactored view student component and added datepicker

---
+++ src/components/Pedometer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pedometer() {
+	return (
+		<div className="pedometer">
+			<div>hello world</div>
+		</div>
+	);
+}
+
+export default Pedometer;
--- src/pages/ViewStudent.jsx
+++ src/pages/ViewStudent.jsx
@@ -1,23 +1,63 @@
-import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import StudentInfo from '../components/StudentInfo';
 import Vitals from '../components/Vitals';
-import HeartRate from './HeartRate';
-import BloodOxygen from './BloodOxygen';
-import Pedometer from './Pedometer';
-import C404 from './404';
+import Pedometer from '../components/Pedometer';
+import prevArrow from '../assets/svg/prev-arrow.svg';
+import nextArrow from '../assets/svg/next-arrow.svg';
+import calender from '../assets/svg/calender.svg';
 
 function ViewStudent(props) {
+	const [index, setIndex] = useState(0);
+	const [value, setValue] = useState(new Date());
+
+	const handlePrev = () =>
+		setIndex((prev) => {
+			const number = prev - 1;
+			if (number < 0) return 2;
+			return number;
+		});
+
+	const handleNext = () =>
+		setIndex((prev) => {
+			const number = prev + 1;
+			if (number > 2) return 0;
+			return number;
+		});
+
 	return (
 		<div className="view-student">
 			<StudentInfo {...props} />
 
-			<div className="routes">
-				<Routes>
-					<Route path="/" element={<HeartRate />} />
-					<Route path="/blood-oxygen" element={<BloodOxygen />} />
-					<Route path="/pedometer" element={<Pedometer />} />
-					<Route path="*" element={<C404 />} />
-				</Routes>
+			<div className="graph">
+				<div className="header">
+					<IconButton onClick={handlePrev}>
+						<img src={prevArrow} alt="previous" />
+					</IconButton>
+					<p>{graphs[index].name}</p>
+					<IconButton onClick={handleNext}>
+						<img src={nextArrow} alt="next" />
+					</IconButton>
+				</div>
+
+				<div className="date-wrapper">
+					<LocalizationProvider dateAdapter={AdapterMoment}>
+						<DatePicker
+							value={value}
+							onChange={(newValue) => {
+								setValue(newValue);
+							}}
+							renderInput={(params) => (
+								<TextField {...params} {...extraParams} />
+							)}
+						/>
+					</LocalizationProvider>
+				</div>
+
+				{graphs[index].component}
 			</div>
 
 			<Vitals />
@@ -26,3 +66,32 @@ function ViewStudent(props) {
 }
 
 export default ViewStudent;
+
+const graphs = [
+	{
+		id: 1,
+		name: 'Pedometer',
+		component: <Pedometer />,
+	},
+	{
+		id: 2,
+		name: 'Heart rate monitor',
+		component: <></>,
+	},
+	{
+		id: 3,
+		name: 'Blood oxygen',
+		component: <></>,
+	},
+];
+
+const extraParams = {
+	size: 'small',
+	InputProps: {
+		endAdornment: (
+			<InputAdornment position="end">
+				<img src={calender} alt="calender" />
+			</InputAdornment>
+		),
+	},
+};
